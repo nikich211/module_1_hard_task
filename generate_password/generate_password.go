0ba2225f226afc714b6f89cae11f279838690a37
grayanng <angel.sizaya@gmail.com>
2025-07-26 22:56:15 +0300

    Add password generation

--- generate_password/generate_password.go
+++ generate_password/generate_password.go
@@ -1,10 +1,27 @@
 package generate_password
 
-// letters - список допустимых символов в пароле
+import (
+	"fmt"
+	"math/rand"
+)
+
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func GeneratePassword(n int) (string, error) {
-	// TODO: ваш код
-
-	panic("implement me")
+	if n < 1 {
+		return "", nil
+	}
+	runes := []rune(letters)
+	var result []rune
+	for i := 0; i < n; i++ {
+		result = append(result, runes[rand.Intn(len(runes))])
+	}
+	return string(result), nil
+}
+func main() {
+	str, err := GeneratePassword(8)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(str)
 }
